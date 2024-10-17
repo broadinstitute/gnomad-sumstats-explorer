d99cd46cc7acd95faf0d3dfd5651bc7093544526
jkgoodrich <33063077+jkgoodrich@users.noreply.github.com>
2024-10-17 08:37:08 -0600

    Add shiny app files and data

---
+++ gnomad_sumstats_explorer/app.py
@@ -0,0 +1,213 @@
+"""Simple Shiny app to explore gnomAD v4.1 summary statistics."""
+from shared import app_dir, df, metrics, POP_NAMES, color_map, gen_anc_order_mapped
+
+import plotly.graph_objects as go
+from plotly.graph_objs import FigureWidget
+from shiny import reactive
+from shiny import ui as sui
+from shiny.express import input, render, ui
+from shinywidgets import render_widget
+
+
+ui.include_css(app_dir / "styles.css")
+ui.page_opts(
+    title=sui.row(
+        sui.column(4, ui.img(src = "gnomad_logo.png", height="75")),
+        sui.column(
+            8,
+            ui.div(
+                {"style": "font-weight: bold; font-size: 30px;"},
+                ui.p("      v4.1 summary stats"),
+            ),
+        ),
+    ),
+    #full_width=True,
+    fillable=True
+)
+
+with ui.sidebar(title="Filter controls"):
+    ui.input_selectize(
+        "select_metric", "Select Metric", choices=metrics, selected="n_non_ref"
+    )
+    ui.input_switch("variant_qc_pass", "Pass variant QC", True)
+    ui.input_selectize(
+        "sex_chr_nonpar_group",
+        "Select autosome/PAR or non-PAR",
+        choices=["autosome_or_par", "x_nonpar", "y_nonpar"],
+        selected="autosome_or_par"
+    )
+    ui.input_selectize(
+        "capture",
+        "Filter by capture intervals",
+        choices=["", "ukb_broad_union", "broad", "ukb", "ukb_broad_intersect"],
+        selected=""
+    )
+    ui.input_selectize(
+        "csq_set",
+        "Filter by CSQ set",
+        choices=["", "non-coding", "coding", "lof"],
+        selected="",
+    )
+    ui.input_selectize(
+        "csq",
+        "Filter by CSQ",
+        choices=[
+            "",
+            "missense_variant",
+            "synonymous_variant",
+            "frameshift_variant",
+            "intergenic_variant",
+            "intron_variant",
+            "splice_region_variant",
+            "stop_gained",
+            "splice_donor_variant",
+            "splice_acceptor_variant",
+        ],
+        selected=""
+    )
+    ui.input_selectize(
+        "loftee_label",
+        "Filter by LOFTEE label",
+        choices=["", "HC", "LC"],
+        selected=""
+    )
+    ui.input_selectize(
+        "loftee_flags",
+        "Filter by LOFTEE flags",
+        choices=["", "no_flags", "with_flags"],
+        selected=""
+    )
+    ui.input_selectize(
+        "max_af",
+        "Filter by max AF",
+        choices=["", "0.0001", "0.001", "0.01"],
+        selected=""
+    )
+
+with ui.layout_column_wrap(fill=False):
+    with ui.value_box():
+        "Number of rows"
+
+        @render.text
+        def count():
+            return filtered_df().shape[0]
+
+    with ui.value_box():
+        "Full dataset mean"
+
+        @render.text
+        def bill_length():
+            return f"{filtered_df_global_mean():.1f}"
+
+
+with ui.layout_columns():
+    with ui.card(full_screen=True):
+        ui.card_header("Per-sample summary statistics distributions")
+
+        #@output(suspend_when_hidden=False)
+        @render_widget
+        def length_depth():
+            filt_df = filtered_df()
+            return create_boxplot_figure(filt_df)
+
+    with ui.card(full_screen=True):
+        ui.card_header("")
+
+        @render.data_frame
+        def summary_statistics():
+            cols = [
+                "subset",
+                "gen_anc",
+                "sex_chr_nonpar_group",
+                "variable",
+                "value",
+            ]
+            return render.DataGrid(filtered_df()[cols], filters=True)
+
+@reactive.calc
+def metric_filtered_df():
+    metric = input.select_metric()
+    variant_qc_pass = "pass" if input.variant_qc_pass() else ""
+    sex_chr_nonpar_group = input.sex_chr_nonpar_group()
+    capture = input.capture()
+    csq_set = input.csq_set()
+    csq = input.csq()
+    loftee_label = input.loftee_label()
+    loftee_flags = input.loftee_flags()
+    max_af = input.max_af()
+    filt_df = df[
+        (df['sex_chr_nonpar_group'] == sex_chr_nonpar_group)
+        & (df['variant_qc'] == variant_qc_pass)
+        & (df['capture'] == capture)
+        & (df['csq_set'] == csq_set)
+        & (df['csq'] == csq)
+        & (df['loftee_label'] == loftee_label)
+        & (df['loftee_flags'] == loftee_flags)
+        & (df['max_af'] == max_af)
+    ]
+    rename_map = {
+        "min": "Minimum",
+        "q25": "Q1",
+        "q50": "Median",
+        "q75": "Q3",
+        "max": "Maximum",
+        "mean": "Mean",
+    }
+    filt_df = filt_df.rename(
+        columns={f"{metric}_{k}": v for k, v in rename_map.items()}
+    )
+
+    id_vars = ["subset", "gen_anc", "sex_chr_nonpar_group", "variant_qc", "capture",
+               "csq_set", "csq", "loftee_label", "loftee_flags", "max_af"]
+    filt_df = filt_df[
+        id_vars + ["Minimum", "Q1", "Median", "Q3", "Maximum", "Mean"]
+    ]
+
+    return filt_df
+
+@reactive.calc
+def filtered_df():
+    #gen_ancs = input.gen_anc()
+    filt_df = metric_filtered_df()
+    filt_df = filt_df.replace({"gen_anc": POP_NAMES})
+    #filt_df = filt_df[filt_df["gen_anc"].isin(gen_ancs)]
+    id_vars = ["subset", "gen_anc", "sex_chr_nonpar_group", "variant_qc", "capture",
+               "csq_set", "csq", "loftee_label", "loftee_flags", "max_af"]
+    filt_df = filt_df.drop(columns=["Mean"])
+    filt_df = filt_df.melt(id_vars=id_vars, value_name="value")
+
+    return filt_df
+
+def filtered_df_global_mean():
+    filt_df = metric_filtered_df()
+    filt_df = filt_df[
+        (filt_df["gen_anc"] == "global") & (filt_df["subset"] == "gnomad")
+    ]
+    filt_df = filt_df.reset_index()
+    return filt_df.at[0, 'Mean']
+
+
+# Function to create the Plotly FigureWidget.
+def create_boxplot_figure(data):
+    fig = go.Figure()
+
+    for gen_anc in gen_anc_order_mapped:
+        fig.add_trace(
+            go.Box(
+                x=data[data["gen_anc"] == gen_anc]["subset"],
+                y=data[data["gen_anc"] == gen_anc]["value"],
+                name=gen_anc,
+                marker_color=color_map[gen_anc],
+                quartilemethod="exclusive"
+            )
+        )
+    fig.update_layout(
+        template="simple_white",
+        legend_title="Genetic Ancestry",
+        xaxis_title="Subset",
+        yaxis_title="Value",
+        showlegend=True,
+        # group together boxes of the different traces for each value of x
+        boxmode='group'
+    )
+    return FigureWidget(fig)
---
+++ gnomad_sumstats_explorer/shared.py
@@ -0,0 +1,117 @@
+"""Shared constants and functions for the gnomAD summary statistics explorer."""
+
+from pathlib import Path
+
+import pandas as pd
+
+app_dir = Path(__file__).parent
+df = pd.read_csv(app_dir / "../data/gnomad.exomes.v4.1.per_sample_variant_counts.csv", keep_default_na=False)
+
+metrics = [
+    "n_non_ref",
+    "n_non_ref_alleles",
+    "n_het",
+    "n_hom_var", 
+    "n_hemi_var",
+    "n_snp",
+    "n_indel",
+    "n_insertion", 
+    "n_deletion",
+    "n_transition",
+    "n_transversion", 
+    "n_singleton", 
+    "n_singleton_ti", 
+    "n_singleton_tv",
+    "n_over_gq_60", 
+    "n_over_dp_20", 
+    "n_over_dp_30",
+    "r_ti_tv", 
+    "r_ti_tv_singleton",
+    "r_het_hom_var", 
+    "r_insertion_deletion",
+    "n_high_ab_het_ref", 
+]
+gen_anc_order = [
+    "global",
+    "afr",
+    "amr",
+    "asj",
+    "eas",
+    "fin",
+    "mid",
+    "nfe",
+    "sas",
+    "remaining",
+]
+
+POP_NAMES = {
+    "afr": "African/African-American",
+    "ami": "Amish",
+    "amr": "Admixed American",
+    "asj": "Ashkenazi Jewish",
+    "eas": "East Asian",
+    "eur": "European",
+    "fin": "Finnish",
+    # NOTE: mde is kept for historical purposes, in gnomAD v3.1 mid was used instead
+    "mde": "Middle Eastern",
+    "mid": "Middle Eastern",
+    "nfe": "Non-Finnish European",
+    "oth": "Other",
+    "remaining": "Remaining individuals",
+    "sas": "South Asian",
+    "uniform": "Uniform",
+    "sas_non_consang": "South Asian (F < 0.05)",
+    "consanguineous": "South Asian (F > 0.05)",
+    "exac": "ExAC",
+    "bgr": "Bulgarian (Eastern European)",
+    "est": "Estonian",
+    "gbr": "British",
+    "nwe": "North-Western European",
+    "seu": "Southern European",
+    "swe": "Swedish",
+    "kor": "Korean",
+    "sgp": "Singaporean",
+    "jpn": "Japanese",
+    "oea": "Other East Asian",
+    "oeu": "Other European",
+    "onf": "Other Non-Finnish European",
+    "unk": "Unknown",
+    "global": "All"
+}
+
+POP_COLORS = {
+    "afr": "#941494",
+    "ami": "#FFC0CB",
+    "amr": "#ED1E24",
+    "asj": "#FF7F50",
+    "eas": "#108C44",
+    "eur": "#6AA5CD",
+    "fin": "#002F6C",
+    "mde": "#33CC33",
+    "nfe": "#6AA5CD",
+    "oth": "#ABB9B9",
+    "sas": "#FF9912",
+    "uniform": "pink",
+    "consanguineous": "pink",
+    "sas_non_consang": "orange",
+    "exac": "gray",
+    "bgr": "#66C2A5",
+    "est": "black",
+    "gbr": "#C60C30",
+    "nwe": "#C60C30",
+    "seu": "#3CA021",
+    "swe": "purple",
+    "kor": "#4891D9",
+    "sgp": "darkred",
+    "jpn": "#BC002D",
+    "oea": "#108C44",
+    "oeu": "#6AA5CD",
+    "onf": "#6AA5CD",
+    "unk": "#ABB9B9",
+    "remaining": "#ABB9B9",
+    "": "#ABB9B9",
+    "mid": "#33CC33",
+    "global": "#000000"
+}
+color_map = {POP_NAMES[x]: POP_COLORS[x] for x in gen_anc_order}
+gen_anc_order_mapped = [POP_NAMES[x] for x in gen_anc_order]
